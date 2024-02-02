4eb78b437154b913b43533142e1f5702119c0f37
Magnilson M <magnilson.moreira@gmail.com>
2024-02-02 17:23:05 -0300

    teste
    add html 2 conteudo duvidoso

--- scripst.js
+++ scripst.js
@@ -1,3 +1,4 @@
+alert('IGNORA TUDO SO CLICA EM ENTRAR')
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
